07ccc695624e6ea1c7d2016a7590c2c0375e2e10
helloals <helloals5236@naver.com>
2025-06-11 00:00:31 +0900

    매출표완

--- script.js
+++ script.js
@@ -40,7 +40,18 @@
     }
     isPaused = !isPaused;
   }
-
+  
+    // 매출 슬라이드
+    let roller = document.querySelector('.roller .rolling_list');
+    roller.id = 'roller1';
+    let clone = roller.cloneNode(true);
+    clone.id = 'roller2';
+    document.querySelector('.roller').appendChild(clone);
+    roller.classList.add('original');
+    clone.classList.add('clone');
+    
+    
+    
   // 버튼 이벤트
   prevBtn.addEventListener("click", () => {
     showPrevSlide();
